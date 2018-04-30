a9ea2e41e04d81167067d0994211b8f4e71870b7
adriano <negrao.adriano@gmail.com>
2018-04-30 14:21:43 -0300

    3.9 validando descrição

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -2,11 +2,18 @@ package com.algaworks.brewer.model;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Size;
+
 public class Cerveja {
 
     @NotBlank
     private String sku;
+
+    @NotBlank
     private String nome;
+
+    @Size(min = 1, max = 50)
+    private String descricao;
 
     public String getSku() {
         return sku;
@@ -22,5 +29,13 @@ public class Cerveja {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
